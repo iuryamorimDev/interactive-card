d0e1a92ef1b0e9d1d607f330bfe769db6796724a
Iury de Amorim Demberg <iuryamorimdev@gmail.com>
2024-04-20 17:31:08 -0400

    done

---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.querySelector(".button");
+let continueButton = document.querySelector(".continue");
+
+let cardHolderName = document.querySelector("cardHolderNameInput");
+let cardHolderNumber = document.querySelector("cardHolderNameInput");
+let month = document.querySelector("month");
+let year = document.querySelector("year");
+let cvc = document.querySelector("cvc");
+
+button.addEventListener("click", function () {});
+
+continueButton.addEventListener("click", function () {});
